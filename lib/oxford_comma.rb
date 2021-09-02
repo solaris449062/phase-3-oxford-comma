266540e8c98793a389d23894d0a80140a4f163d9
Sungoh Park <spark449062@gmail.com>
2021-09-02 09:32:10 -0600

    commit tree c8e4864b3940ac3de100e46b8d4525191945fc29

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    string = ""
+    if array.length == 1
+        string += "#{array[0]}"
+    elsif array.length == 2
+        string += "#{array[0]} and #{array[1]}"
+    else
+        for i in 0..(array.length - 1)
+            if i != array.length - 1
+                string += "#{array[i]}, "
+            else 
+                string += "and #{array[i]}"
+            end
+        end 
+    end
+    return string
 end
+
+# oxford_comma(["a", "b", "c", "d", "e"])
